9778e498d07348e965d09f785541a54d8b4a3a0f
SapnagBagde <sapna2001bagde@gmail.com>
2024-06-02 21:53:35 +0530

    add defaultcard

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import {Link} from  "react-router-dom"
 import LogoImage from "../images/Logo.png"
+import { DefaultCard } from '../components/DefaultCard'
 
 
 import {
